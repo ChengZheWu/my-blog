475cac679d97b9220df0f8f9ccfa6c347211e39f
andy <luckyandy234@gmail.com>
2026-02-03 19:02:42 +1100

    v3: use my dns &  https

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,7 +18,59 @@ resource "hcloud_server" "k3s_node" {
   user_data = templatefile("${path.module}/install_k3s.tftpl", {
     k8s_yaml_content = join("\n---\n", [
         file("${path.module}/k8s/deployment.yaml"),
-        file("${path.module}/k8s/service.yaml")
+        file("${path.module}/k8s/service.yaml"),
+        file("${path.module}/k8s/ingress.yaml")
     ])
   })
+}
+
+resource "cloudflare_dns_record" "root" {
+  zone_id = var.cloudflare_zone_id
+  name = "@"
+  ttl = 1
+  content = hcloud_server.k3s_node.ipv4_address
+  type = "A"
+  proxied = true
+}
+
+resource "hcloud_firewall" "k3s_firewall" {
+  name = "k3s-security-group"
+
+  # SSH
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "22"
+    source_ips = [
+      "0.0.0.0/0", # 開發中，之後再改為我的IP
+      "::/0"
+    ]
+  }
+
+  # HTTP
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  # HTTPS
+  rule {
+    direction = "in"
+    protocol = "tcp"
+    port = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_firewall_attachment" "main" {
+  firewall_id = hcloud_firewall.k3s_firewall.id
+  server_ids = [hcloud_server.k3s_node.id]
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -21,3 +21,15 @@ variable "ssh_public_key_path" {
   type = string
   default = "~/.ssh/id_rsa.pub"
 }
+
+variable "cloudflare_api_token" {
+  description = "Cloudflare API Token"
+  type = string
+  sensitive = true
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type = string
+  sensitive = true
+}
